9cb1eeb5a191c598c2f82e68780121c91692d082
UncSald <iikka.alingue@gmail.com>
2024-12-05 22:34:32 +0200

    Exercises 2.6-2.10.

---
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,139 @@
+import { useState } from 'react'
+
+const Person = (props) => {
+  return (
+    <p>
+      {props.name} {props.number}
+    </p>
+  )
+}
+
+const ListPerson = ({numbersToShow}) => {
+  return (
+    <>
+      {numbersToShow.map(person =>
+        <Person
+          key={person.name} 
+          name={person.name}
+          number={person.number}/>
+        )
+      }
+    </>
+  )
+}
+
+const Filter = ({numbersShown,setShowNumbers}) => {
+  const handleFilter = (event) => {
+    setShowNumbers(event.target.value)
+  }
+  return (
+    <div>
+        filter: 
+        <input
+          value={numbersShown}
+          onChange={handleFilter}
+        />
+      </div>
+  )
+}
+
+const PersonForm = ({persons, setPersons, newName, setNewName, newNumber, setNewNumber}) => {
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const nameInBook = (newName) => {
+    const found = persons.find((props) => props.name === newName)
+    return found
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    
+    if (nameInBook(newName) != undefined) {
+      alert(`${newName} is already added to phonebook`)
+    }
+    else {
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: String(persons.length + 1),
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+  }
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: 
+        <input
+          value={newName}
+          onChange={handleNameChange}
+        />
+      </div>
+      <div>
+        number:
+        <input
+        value={newNumber}
+        onChange={handleNumberChange}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const App = () => {
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [numbersShown, setShowNumbers] = useState('')
+
+  const numbersToShow = numbersShown === ''
+  ? persons
+  : persons.filter(person => person.name.toLowerCase().match(numbersShown.toLowerCase()))
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        <Filter numbersShown={numbersShown}
+          setShowNumbers={setShowNumbers}
+        />
+        <h2>add new</h2>
+        <PersonForm
+          persons={persons}
+          setPersons={setPersons}
+          newName={newName}
+          setNewName={setNewName}
+          newNumber={newNumber}
+          setNewNumber={setNewNumber}
+        />
+
+        <h2>Numbers</h2>
+        <div>
+          <ListPerson numbersToShow={numbersToShow}/>
+        </div>
+      </div>
+
+    </div>
+  )
+
+}
+
+export default App
