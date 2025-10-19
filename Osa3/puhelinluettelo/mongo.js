8c9704caf5048d474bde4b179a69bbad66333ee7
UncSald <iikka.alingue@gmail.com>
2025-10-19 22:29:41 +0300

    Exercise 3.12

---
+++ Osa3/puhelinluettelo/mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+
+const url = `mongodb+srv://MacouJr:${password}@cluster0.x1dpvwn.mongodb.net/personsApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personsSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personsSchema)
+
+if (process.argv.length == 3) {
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+    result.forEach(person => {
+        console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+    })
+} else {
+    const name = process.argv[3]
+    const number = process.argv[4]
+
+    const person = new Person({
+    name: `${name}`,
+    number: `${number}`,
+    })
+
+    person.save().then(result => {
+    console.log(`Added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+    })
+}
+
+
+
