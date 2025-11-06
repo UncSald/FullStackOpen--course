ebd57baf6d6ad967089469bf32c3c3e9aade7b23
UncSald <iikka.alingue@gmail.com>
2025-11-06 14:51:24 +0200

    Update on ex 4.2

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -23,8 +23,4 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 
-app.listen(config.PORT, () => {
-    console.log(`Server running on port ${config.PORT}`)
-})
-
 module.exports = app
