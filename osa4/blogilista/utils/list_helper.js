318e5726658fda448ac2a8c9ba013ef268931b29
UncSald <iikka.alingue@gmail.com>
2025-11-17 13:57:12 +0200

    added some tests for total likes. created favorite blogs and tests.

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,8 +6,13 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const favourite = blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max), blogs[0])
+    return favourite
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
