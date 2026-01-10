914dbf1137923a904b35f2600246f19eae16fab7
UncSald <iikka.alingue@gmail.com>
2026-01-10 10:50:01 +0200

    Added form and method to create blogs. Ex 5.3

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -9,6 +9,10 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
 
 
   useEffect(() => {
@@ -36,6 +40,7 @@ const App = () => {
       setUser(user)
       setUsername('')
       setPassword('')
+      blogService.setToken(user.token)
     } catch {
       setErrorMessage('wrong credentials')
       setTimeout(() => {
@@ -47,6 +52,16 @@ const App = () => {
   const handleLogout = async event => {
     event.preventDefault()
     window.localStorage.clear()
+  }
+
+  const handleBlogCreation = async event => {
+    event.preventDefault()
+    const blog = {
+      title: title,
+      author: author,
+      url: url
+    }
+    await blogService.create(blog)
   }
 
   const logInForm = () => {
@@ -81,13 +96,49 @@ const App = () => {
     )
   }
 
+  const newBlogForm = () => {
+    return (
+      <div>
+        <form onSubmit={handleBlogCreation}>
+          <div>
+            <label>
+              title
+              <input
+                type="text"
+                value={title}
+                onChange={({ target }) => setTitle(target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              author
+              <input
+                type="text"
+                value={author}
+                onChange={({ target }) => setAuthor(target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              url
+              <input
+                type="url"
+                value={url}
+                onChange={({ target }) => setUrl(target.value)}
+              />
+            </label>
+          </div>
+          <button type="submit">create</button>
+        </form>
+      </div>
+    )
+  }
+
   const showBlogs = () => {
     return (
       <div>
-          <h2>blogs</h2>
-          <p>{user.name} logged in
-          <button type="logout" onClick={handleLogout}>logout</button>
-          </p>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
@@ -100,7 +151,18 @@ const App = () => {
   return (    
     <div>
       {!user && logInForm()}
-      {user && showBlogs()}
+      {user && (
+        <div>
+          <h2>blogs</h2>
+          <p>
+            {user.name} logged in
+            <button type="logout" onClick={handleLogout}>logout</button>
+          </p>
+          {newBlogForm()}
+          {showBlogs()}
+        </div>
+        )
+       }
 
     </div>
   )}
