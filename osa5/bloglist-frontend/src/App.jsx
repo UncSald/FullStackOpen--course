09aac4e69535782246d7b89352bdf1d55af2782e
UncSald <iikka.alingue@gmail.com>
2026-01-10 10:18:53 +0200

    Added token to local storage and logout button. Ex 5.2

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -17,19 +17,36 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async event => {
-  event.preventDefault()
-  try {
-    const user = await loginService.login({ username, password })
-    setUser(user)
-    setUsername('')
-    setPassword('')
-  } catch {
-    setErrorMessage('wrong credentials')
-    setTimeout(() => {
-      setErrorMessage(null)
-    }, 5000)
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
+
+  const handleLogout = async event => {
+    event.preventDefault()
+    window.localStorage.clear()
   }
 
   const logInForm = () => {
@@ -68,7 +85,9 @@ const App = () => {
     return (
       <div>
           <h2>blogs</h2>
-          <p>{user.name} logged in</p>
+          <p>{user.name} logged in
+          <button type="logout" onClick={handleLogout}>logout</button>
+          </p>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}
@@ -78,9 +97,7 @@ const App = () => {
 
 
 
-  return (
-
-    
+  return (    
     <div>
       {!user && logInForm()}
       {user && showBlogs()}
