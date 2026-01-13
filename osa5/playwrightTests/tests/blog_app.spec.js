38831b77e9ead72ca59db1d5b44319457a58b6de
UncSald <iikka.alingue@gmail.com>
2026-01-13 15:53:13 +0200

    Added test to see if it is possible to log in with correct credentials and impossible to login with incorrect credentials. Ex 5.18

--- osa5/playwrightTests/tests/blog_app.spec.js
+++ osa5/playwrightTests/tests/blog_app.spec.js
@@ -2,6 +2,7 @@ const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
+    await page.goto('http://localhost:3003')
     await request.post('http://localhost:3003/api/testing/reset')
     await request.post('http://localhost:3003/api/users', {
       data: {
@@ -10,17 +11,33 @@ describe('Blog app', () => {
         password: 'salainen'
       }
     })
-    await page.goto('http://localhost:3003')
     await page.goto('http://localhost:5173')
   })
 
   test('Login form is shown', async ({ page }) => {
-    await page.goto('http://localhost:5173')
 
     const locator = page.getByText('log in to application')
-    console.log(locator)
     await expect(locator).toBeVisible()
     await expect(page.getByText('username')).toBeVisible()
     await expect(page.getByText('password')).toBeVisible()
   })
+
+  describe('Login', () => {
+
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByRole('button', { name: 'login' }).click()
+      await page.getByLabel('username').fill('mluukkai')
+      await page.getByLabel('password').fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    })
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByRole('button', { name: 'login' }).click()
+      await page.getByLabel('username').fill('macou')
+      await page.getByLabel('password').fill('salakavala')
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('log in to application')).toBeVisible()
+    })
+  })
+
 })
