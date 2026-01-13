ad9ab51c8907b6678eb6a29ab0c5b13eec8a74af
UncSald <iikka.alingue@gmail.com>
2026-01-13 15:32:28 +0200

    Created another npm project for playwright and created a test for the blog app. Test sees if the login page can be opened. Ex 5.17

---
+++ osa5/playwrightTests/tests/blog_app.spec.js
@@ -0,0 +1,26 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    const locator = page.getByText('log in to application')
+    console.log(locator)
+    await expect(locator).toBeVisible()
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
+  })
+})
