a24d7b0ff3c51e4f218cca8c125d6fde57d320c3
UncSald <iikka.alingue@gmail.com>
2026-01-13 14:06:46 +0200

    Added test for clicking the show button. Blog should show blog info. Ex 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -13,4 +14,31 @@ test('renders content', () => {
 
   const element = screen.getByText('Canonical string reduction Edsger W. Dijkstra')
   expect(element).toBeDefined()
+})
+
+test('clicking the show button opens blog info', async () => {
+  const blog = {
+  title: "Canonical string reduction",
+  author: "Edsger W. Dijkstra",
+  url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+  likes: 12,
+  user: {
+    username: 'macou',
+    name:'macou'
+  }
+  }
+
+  render(
+    <Blog blog={blog}/>
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('show')
+  await user.click(button)
+
+  const url = screen.getByText('http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html')
+  const likes = screen.getByText('likes 12')
+  const username = screen.getByText('macou')
+
+  expect(url, likes, username).toBeDefined()
 })
