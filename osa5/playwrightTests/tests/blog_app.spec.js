caeeaab1973b201f13cfb2c33d68d78c3b60c646
UncSald <iikka.alingue@gmail.com>
2026-01-14 14:03:45 +0200

    Added test to see if the most liked blog is shown first on the blog list. Ex 5.23. Cleaned code with helper functions.

--- osa5/playwrightTests/tests/blog_app.spec.js
+++ osa5/playwrightTests/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith, createBlog, showBlogInfo } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -31,72 +32,85 @@ describe('Blog app', () => {
   describe('Login', () => {
 
     test('succeeds with correct credentials', async ({ page }) => {
-      await page.getByRole('button', { name: 'login' }).click()
-      await page.getByLabel('username').fill('mluukkai')
-      await page.getByLabel('password').fill('salainen')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, 'mluukkai', 'salainen')
       await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
     })
     test('fails with wrong credentials', async ({ page }) => {
-      await page.getByRole('button', { name: 'login' }).click()
-      await page.getByLabel('username').fill('macou')
-      await page.getByLabel('password').fill('salakavala')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, 'macou', 'salakavala')
       await expect(page.getByText('log in to application')).toBeVisible()
     })
   })
 
   describe('When logged in', () => {
     beforeEach(async ({ page }) => {
-      await page.getByLabel('username').fill('mluukkai')
-      await page.getByLabel('password').fill('salainen')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, 'mluukkai', 'salainen')
     })
     
     test('a new blog can be created', async ({ page }) => {
-      await page.getByRole('button', { name: 'create new blog' }).click()
-      await page.getByLabel('title').fill('First class tests')
-      await page.getByLabel('author').fill('Robert C. Martin')
-      await page.getByLabel('url').fill('http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll')
-      await page.getByRole('button', { name: 'create' }).click()
+      await createBlog(page,
+        'First class tests',
+        'Robert C. Martin',
+        'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll')
+
       await expect(page.getByText('First class tests Robert C. Martin')).toBeVisible()
     })
   })
 
-  describe('When there are blogs on the list', () => {
+  describe('When there is a blog on the list', () => {
     beforeEach(async ({ page }) => {
-      await page.getByLabel('username').fill('mluukkai')
-      await page.getByLabel('password').fill('salainen')
-      await page.getByRole('button', { name: 'login' }).click()
-      await page.getByRole('button', { name: 'create new blog' }).click()
-      await page.getByLabel('title').fill('First class tests')
-      await page.getByLabel('author').fill('Robert C. Martin')
-      await page.getByLabel('url').fill('http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll')
-      await page.getByRole('button', { name: 'create' }).click()
+      await loginWith(page, 'mluukkai', 'salainen')
+      await createBlog(page,
+        'First class tests',
+        'Robert C. Martin',
+        'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll')
     })
     test('a blog can be liked', async ({ page }) => {
-      await page.getByRole('button', { name: 'show' }).click()
+      await showBlogInfo(page, 'First class tests', 'Robert C. Martin')
       await page.getByRole('button', { name: 'like' }).click()
       await expect(page.getByText('likes 1')).toBeVisible()
     })
     test('a blog can be deleted by the creator', async ({ page }) => {
-      await page.getByRole('button', { name: 'show' }).click()
+      await showBlogInfo(page, 'First class tests', 'Robert C. Martin')
       await page.getByRole('button', { name: 'delete' }).click()
       page.on('dialog', dialog => dialog.accept())
       await expect(page.getByText('successfully deleted First class tests')).toBeVisible()
       await expect(page.getByText('First class tests Robert C. Martin')).toBeHidden()
     })
     test('the delete button can only be seen by the creator', async ({ page }) => {
-      await page.getByRole('button', { name: 'show' }).click()
+      await showBlogInfo(page, 'First class tests', 'Robert C. Martin')
       await expect(page.getByRole('button', { name: 'delete' })).toBeVisible()
       await page.getByRole('button', { name: 'logout' }).click()
       await expect(page.getByText('log in to application')).toBeVisible()
-      await page.getByLabel('username').fill('macou')
-      await page.getByLabel('password').fill('salasana')
-      await page.getByRole('button', { name: 'login' }).click()
-      await page.getByRole('button', { name: 'show' }).click()
+      await loginWith(page, 'macou', 'salasana')
+      await expect(page.getByText('Marc Alingue logged in')).toBeVisible()
+      await showBlogInfo(page, 'First class tests', 'Robert C. Martin')
       await expect(page.getByRole('button', { name: 'delete' })).toBeHidden()
     })
+
+    describe('when there are multiple blogs on the list', () => {
+      beforeEach(async ({ page }) => {
+        await createBlog(page,
+          'Canonical string reduction',
+          'Edsger W. Dijkstra',
+          'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html')
+      })
+      test('blogs are ordered with the most liked blog first', async ({ page }) => {
+        let blogsList = page.locator('div > li')
+        let expectedOrder = ['First class tests Robert C. Martinshow',
+          'Canonical string reduction Edsger W. Dijkstrashow']
+        await expect(blogsList).toHaveText(expectedOrder)
+        await showBlogInfo(page, 'Canonical string reduction', 'Edsger W. Dijkstra')
+        const likeButton = page.getByRole('button', { name: 'like' })
+        await likeButton.click()
+        await page.goto('http://localhost:5173')
+        blogsList = page.locator('div > li')
+        expectedOrder = ['Canonical string reduction Edsger W. Dijkstrashow',
+          'First class tests Robert C. Martinshow']
+        await expect(blogsList).toHaveText(expectedOrder)
+      })
+    })
   })
+
+
 
 })
